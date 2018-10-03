4140745ba3fc1dfff247e3c5e53b2606f6364bea
Luiz Paulo Gonçalves <37484860+LuizPauloLPG2@users.noreply.github.com>
2018-10-03 17:33:06 -0300

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -2,8 +2,5 @@
 
 require_once ("./conexao.php");
 
-$sql = ("select * from tbprofissional");
-$stmt = Db::_conexao()->prepare($sql);
-$stmt->execute();
-$stmt = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($stmt);
+$sql = Db::_conexao()->query("select * from tbprofissional")->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($sql);
